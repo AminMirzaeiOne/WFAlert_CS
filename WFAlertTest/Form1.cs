0aaa38610c9ccdd15e34d15bb28f4ff762b8191e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-25 00:20:08 +0330

    - Optimized DPI Test Project

--- WFAlertTest/Form1.cs
+++ WFAlertTest/Form1.cs
@@ -19,7 +19,7 @@ namespace WFAlertTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            WFAlert.AlertMessage.Show("Hello World", WFAlert.Types.Success);
         }
     }
 }
