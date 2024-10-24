d9344f39d50d3780db508583e05f437f4aef272d
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-25 01:14:11 +0330

    - Add Sounds To WFAlert Library Resources

--- WFAlertTest/Form1.Designer.cs
+++ WFAlertTest/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button4
@@ -40,7 +44,7 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(157, 319);
+            this.button4.Location = new System.Drawing.Point(24, 288);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(204, 58);
             this.button4.TabIndex = 1;
@@ -54,7 +58,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(157, 255);
+            this.button3.Location = new System.Drawing.Point(24, 202);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(204, 58);
             this.button3.TabIndex = 2;
@@ -68,7 +72,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(157, 191);
+            this.button2.Location = new System.Drawing.Point(24, 121);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(204, 58);
             this.button2.TabIndex = 3;
@@ -82,7 +86,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(157, 127);
+            this.button1.Location = new System.Drawing.Point(24, 41);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(204, 58);
             this.button1.TabIndex = 4;
@@ -90,18 +94,74 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Fill",
+            "Border"});
+            this.comboBox1.Location = new System.Drawing.Point(346, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 36);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(262, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Style :";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Light",
+            "Dark"});
+            this.comboBox2.Location = new System.Drawing.Point(346, 107);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(204, 36);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(253, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Theme :";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 502);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(605, 401);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WFAlert Test";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,6 +171,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WFAlertTest/Form1.cs
+++ WFAlertTest/Form1.cs
@@ -33,14 +33,38 @@ namespace WFAlertTest
 
         private void button3_Click(object sender, EventArgs e)
         {
-            WFAlert.AlertMessage.Show("Success Message", WFAlert.Types.Error,false);
+            WFAlert.AlertMessage.Show("Success Message", WFAlert.Types.Error,true);
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            WFAlert.AlertMessage.Show("Warning Message", WFAlert.Types.Info,false);
+            WFAlert.AlertMessage.Show("Warning Message", WFAlert.Types.Info,true);
 
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.comboBox2.SelectedIndex == 0)
+            {
+                WFAlert.AlertMessage.Theme = WFAlert.Themes.Light;
+            }
+            else
+            {
+                WFAlert.AlertMessage.Theme = WFAlert.Themes.Dark;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.comboBox1.SelectedIndex == 0)
+            {
+                WFAlert.AlertMessage.Style = WFAlert.Styles.Fill;
+            }
+            else
+            {
+                WFAlert.AlertMessage.Style = WFAlert.Styles.Border;
+            }
         }
     }
 }
