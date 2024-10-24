0fb3c26f3ee2fdcb2ede6237147c9ceb11d862b9
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-25 01:28:42 +0330

    - Create and Complete StreamSound Method In AlertSounds Struct

--- WFAlert/AlertMessage.cs
+++ WFAlert/AlertMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -41,16 +42,49 @@ namespace WFAlert
 
     public static class AlertMessage
     {
+        
+
         public static WFAlert.Themes Theme { get; set; } = WFAlert.Themes.Light;
         public static WFAlert.Styles Style { get; set; } = WFAlert.Styles.Border;
         public static Font Font { get; set; } = new Font("Segoe UI", 10, FontStyle.Regular);
 
         public struct AlertSounds
         {
+            private static System.IO.Stream soundLocation;
+            private static System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer();
+
             public static WFAlert.Sounds Success { get; set; } = Sounds.ASound;
             public static WFAlert.Sounds Error { get; set; } = Sounds.BSound;
             public static WFAlert.Sounds Info { get; set; } = Sounds.CSound;
             public static WFAlert.Sounds Warning { get; set; } = Sounds.DSound;
+
+            private void StreamSound(WFAlert.Sounds sound)
+            {
+                switch (sound) 
+                {
+                    case Sounds.ASound:
+                        soundLocation = Properties.Resources.ASound;
+                        break;
+                    case Sounds.BSound:
+                        soundLocation = Properties.Resources.BSound;
+                        break;
+                    case Sounds.CSound:
+                        soundLocation = Properties.Resources.CSound;
+                        break;
+                    case Sounds.DSound:
+                        soundLocation = Properties.Resources.DSound;
+                        break;
+                    case Sounds.ESound:
+                        soundLocation = Properties.Resources.ESound;
+                        break;
+                    case Sounds.FSound:
+                        soundLocation = Properties.Resources.FSound;
+                        break;
+                    case Sounds.GSound:
+                        soundLocation = Properties.Resources.GSound;
+                        break;
+                }
+            }
         }
 
         public static void Show(string message, WFAlert.Types type, bool closebutton = true)
